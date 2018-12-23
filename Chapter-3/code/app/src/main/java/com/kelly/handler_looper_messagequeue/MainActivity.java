d743b30e800b2511662e49c72eb7c3809ad45db8
kelly <imaxthon@163.com>
2018-12-23 10:50:39 +0800

    add android message queue

---
+++ Chapter-3/code/app/src/main/java/com/kelly/handler_looper_messagequeue/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kelly.handler_looper_messagequeue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
